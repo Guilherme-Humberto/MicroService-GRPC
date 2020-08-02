449dd59709807bb75b937b4864dc596bc3c68e7d
Guilherme Humberto <gbreu@outlook.com>
2020-08-02 02:56:39 -0300

    Defindo routas da aplicação

---
+++ my-app-api/src/routes/index.js
@@ -0,0 +1,12 @@
+const express = require('express');
+const router = express.Router();
+
+const implementationUsers = require('../controllers/users');
+const implementationSessions = require('../controllers/sessions');
+
+// Defindo as rotas e implementando as funções dos controllres
+router.get('/users/:id', implementationUsers.show)
+router.post('/create', implementationUsers.create)
+router.post('/sessions', implementationSessions.login)
+
+module.exports = router;
