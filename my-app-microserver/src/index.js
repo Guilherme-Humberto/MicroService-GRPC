63516d9bc58aec4638b40b5b29c6fe6ef796e520
Guilherme Humberto <gbreu@outlook.com>
2020-08-02 03:08:17 -0300

    Configurando protoloader e definindo servidor GRPC

---
+++ my-app-microserver/src/index.js
@@ -0,0 +1,21 @@
+const grpc = require('grpc');
+const path = require('path')
+const protoLoader = require('@grpc/proto-loader');
+const implementation = require('./controllers')
+const loaderConfig = require('./config/index')
+
+require('./db/connetion/index')
+require('./db/schemas/index')
+
+// Configurção para carregar arquivos .proto
+const packageDefinition = protoLoader.loadSync(
+path.resolve(__dirname, 'pb', 'contract_server.proto'),
+    loaderConfig
+);
+const proto = grpc.loadPackageDefinition(packageDefinition);
+
+// Abrindo servidor do microserviço
+const server = new grpc.Server();
+server.addService(proto.UserService.service, implementation)
+server.bind('0.0.0.0:3333', grpc.ServerCredentials.createInsecure());
+server.start();
